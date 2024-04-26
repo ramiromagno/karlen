42419b4c0e3c85a9075d851fddc8d91baef91eee
Ramiro Magno <rmagno@pattern.institute>
2024-04-27 00:23:11 +0100

    Update `karlen` docs

--- R/data.R
+++ R/data.R
@@ -21,14 +21,16 @@
 #' the names used in the column `target` where actual murine gene symbols are
 #' used.}
 #' \item{`well`}{Well identifier, i.e. the position within the 96-well plate.}
-#' \item{`dye`}{Type of fluorescence dye, in this data set it is always SYBR
-#' Green I master mix (Roche) (`"SYBR"`).}
 #' \item{`target`}{Target identifier: murine gene symbol where the amplicon maps
 #' to.}
+#' \item{`dye`}{Type of fluorescence dye, in this data set it is always SYBR
+#' Green I master mix (Roche) (`"SYBR"`).}
+#' \item{`sample`}{Name of the biological sample.}
 #' \item{`sample_type`}{Sample type. Most reactions in this data set are
 #' standard curves, i.e. `"std"`, but a few no template controls (`"ntc"`) are
 #' also included.}
 #' \item{`replicate`}{Replicate identifier: 1 thru 5.}
+#' \item{`copies`}{Standard copy number.}
 #' \item{`dilution`}{Dilution factor. Higher number means greater dilution, e.g.
 #' `10` means a 1:10 (ten-fold) dilution.}
 #' \item{`cycle`}{PCR cycle.}
