385babcdf4fa53041f0dfea70d884e10ab9e6ba6
Ramiro Magno <rmagno@pattern.institute>
2024-04-26 21:32:15 +0100

    Whitelist words

---
+++ tools/dic.R
@@ -0,0 +1,18 @@
+words <-
+  c(
+    "Cav",
+    "Eln",
+    "Hspg",
+    "Karlen",
+    "Rpl",
+    "Serpine",
+    "et",
+    "al",
+    "murine",
+    "polymerase",
+    "qPCR",
+    "PCR",
+    "amplicon",
+    "amplicons"
+  )
+saveRDS(words, file = ".aspell/custom_dic.rds", version = 2)
